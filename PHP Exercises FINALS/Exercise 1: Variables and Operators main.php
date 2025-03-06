ce7fc83a36a407a16219c32fa666aa04e53c910d
pr1mo-cntrl <jfd5349@students.uc-bcf.edu.ph>
2025-03-06 07:50:17 +0000

    pia

---
+++ PHP Exercises FINALS/Exercise 1: Variables and Operators main.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 15;
+$b = 7;
+
+echo "a = $a, b = $b\n"; 
+
+echo "Sum (a + b):" .($a + $b) . "\n";
+echo "Difference (a - b): " . ($a - $b) . "\n";
+echo "Product (a * b): " . ($a * $b) . "\n";
+echo "Quotient (a / b): " . ($a / $b) . "\n";
+?>
